ca9076404618d6fafbd633682d21d525bfd607e2
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2026-02-05 09:25:40 +0300

    drf_yasg

--- vehicle/views.py
+++ vehicle/views.py
@@ -11,12 +11,20 @@ from vehicle.serliazers import CarSerializer, MotoSerializer, MilageSerializer, 
 
 
 class CarViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for cars
+    """
+
     serializer_class = CarSerializer
     queryset = Car.objects.all()
     permission_classes = [AllowAny]
 
 
 class MotoCreateAPIView(generics.CreateAPIView):
+    """
+    Moto create endpoint
+    """
+
     serializer_class = MotoCreateSerializer
     permission_classes = [IsAuthenticated]
 
